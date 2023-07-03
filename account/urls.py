f910953168ebce4e23ada5ff3997f34b75b9434c
Dawid <dawidfirmowski@gmail.com>
2023-07-03 15:07:56 +0200

    requirements file, email verification after succesful register

--- account/urls.py
+++ account/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
 	path('logout/', LogoutView.as_view(), name='logout'),
 	path('register/', RegisterView.as_view(), name='register'),
 	path('user/edit', EditUserView.as_view(), name='user-edit'),
+	path('activate/<uidb64>/<token>/', ActivateUserView.as_view(), name='activate'),
 ]
