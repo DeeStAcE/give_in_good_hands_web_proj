c29bd60a5017655f3ef574f7d69071aa4d2d4527
Dawid <dawidfirmowski@gmail.com>
2023-07-05 19:31:37 +0200

    deploy

--- main_project/settings.py
+++ main_project/settings.py
@@ -84,14 +84,14 @@ WSGI_APPLICATION = "main_project.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "good_hands",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'good_hands',
         'USER': 'dawidadmin',
         'PASSWORD': os.getenv("DB_PASSWORD"),
         'HOST': os.getenv("DB_HOST"),
         'PORT': 5432,
-        'OPTIONS': {'sslmode': 'require'}
+        'OPTIONS': {'sslmode': 'require'},
     }
 }
 
